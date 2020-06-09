d039e938c2fba07e4a8b0a0d4f48620e47593bd2
Tordu5 <da_mekk@web.de>
2020-06-09 15:05:16 +0200

    chapter 4 finished

--- src/main/java/de/dpunkt/myaktion/controller/ListCampaignsController.java
+++ src/main/java/de/dpunkt/myaktion/controller/ListCampaignsController.java
@@ -3,8 +3,10 @@ package de.dpunkt.myaktion.controller;
 import java.io.Serializable;
 
 import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import de.dpunkt.myaktion.data.CampaignProducer;
 import de.dpunkt.myaktion.model.Campaign;
 
 @SessionScoped
@@ -12,27 +14,38 @@ import de.dpunkt.myaktion.model.Campaign;
 public class ListCampaignsController implements Serializable {
     private static final long serialVersionUID = 8693277383648025822L;
 
+    @Inject
+    private CampaignProducer campaignProducer;
+
+    private Campaign campaignToDelete;
+
     public String doAddCampaign() {
-        System.out.println("Add Campaign");
+        campaignProducer.prepareAddCampaign();
         return Pages.EDIT_CAMPAIGN;
     }
 
     public String doEditCampaign(Campaign campaign) {
-        System.out.println("Edit Campaign " + campaign);
+        campaignProducer.prepareEditCampaign(campaign);
         return Pages.EDIT_CAMPAIGN;
     }
 
     public String doEditDonationForm(Campaign campaign) {
-        System.out.println("Edit Donation Form of Campaign " + campaign);
+        campaignProducer.setSelectedCampaign(campaign);
         return Pages.EDIT_DONATION_FORM;
     }
 
     public String doListDonations(Campaign campaign) {
-        System.out.println("List Donations of Campaign " + campaign);
+        campaignProducer.setSelectedCampaign(campaign);
         return Pages.LIST_DONATIONS;
     }
 
     public void doDeleteCampaign(Campaign campaign) {
-        System.out.println("Deletion not implemented, yet!");
+        this.campaignToDelete = campaign;
+        System.out.println("Aktion zum löschen vorgemerkt");
     }
+
+    public void commitDeleteCampaign() {
+        System.out.println("Aktion löschen noch nicht implementiert");
+    }
+
 }
